1ac94eb919ce5d82a8af33c19f3bb6df28a73d7c
Abhishek Purohit <abhishekpurohit.om@gmail.com>
2015-10-14 01:24:04 +0530

    commit

---
+++ SimpleIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleIOS
+//
+//  Created by Abhishek on 14/10/15.
+//  Copyright (c) 2015 Abhishek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleIOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleIOS
+//
+//  Created by Abhishek on 14/10/15.
+//  Copyright (c) 2015 Abhishek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
